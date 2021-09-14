c813d815e96a6af2cf666833e75853ed838c3d3c
AndreyKhoroshiy <AndreyKhoroshiy@github.com>
2021-09-15 00:36:46 +0300

    Adding the code for working with the webcam

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ import cv2
 #
 # cv2.waitKey(0)
 
-cap = cv2.VideoCapture('videos/Police.mp4')
+# cap = cv2.VideoCapture('videos/Police.mp4')
+#
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow('Result', img)
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 500)
+cap.set(4, 300)
 
 while True:
     success, img = cap.read()

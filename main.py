0377933034a4761931837df92de1fb308bf663a6
AndreyKhoroshiy <AndreyKhoroshiy@github.com>
2021-09-17 00:23:05 +0300

    Working with a picture:blur, change the format, crop the image when outputting

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import cv2
 
-# img = cv2.imread('images/python.jpeg')
-# cv2.imshow('Result', img)
-#
-# cv2.waitKey(0)
+img = cv2.imread('images/python.jpeg')
+img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+
+# Добавляем размытие ((3, 3) - устанавливать только нечётные значения, иначе будет ошибка)
+img = cv2.GaussianBlur(img, (3, 3), 0)
+
+# Приведение картинки к другому формату, вместо RGB - чёрно-белый
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# можно обрезать картинку при выводе, после ('Result', img[0:100, 0:150])
+cv2.imshow('Result', img)
+# print(img.shape)
+
+cv2.waitKey(0)
+
+
+
 
 # cap = cv2.VideoCapture('videos/Police.mp4')
 #
@@ -14,13 +27,13 @@ import cv2
 #     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         break
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 500)
-cap.set(4, 300)
-
-while True:
-    success, img = cap.read()
-    cv2.imshow('Result', img)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# cap = cv2.VideoCapture(0)
+# cap.set(3, 500)
+# cap.set(4, 300)
+#
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow('Result', img)
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break

ebeb780b9a736ab1e832132f8d2fa13dec89304e
AndreyKhoroshiy <AndreyKhoroshiy@github.com>
2021-09-17 01:03:08 +0300

    Working with the image: finding the corners of the image, increasing and decreasing the width of the stroke of paths

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
+import numpy as np
 
-img = cv2.imread('images/python.jpeg')
+img = cv2.imread('images/Frog.jpg')
 img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
 
 # Добавляем размытие ((3, 3) - устанавливать только нечётные значения, иначе будет ошибка)
@@ -8,6 +9,16 @@ img = cv2.GaussianBlur(img, (3, 3), 0)
 
 # Приведение картинки к другому формату, вместо RGB - чёрно-белый
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Нахождение углов изображения
+img = cv2.Canny(img, 150, 150)
+
+# Увеличение ширины обводки контуров
+kernel = np.ones((5, 5), np.uint8)
+img = cv2.dilate(img, kernel, iterations=1)
+
+# Уменьшение жирности линий обводки
+img = cv2.erode(img, kernel, iterations=1)
 
 # можно обрезать картинку при выводе, после ('Result', img[0:100, 0:150])
 cv2.imshow('Result', img)
